1ec3243a479c0f9d2221b6459c61ce3337ab1635
fernando-cavalcanti <fafhcavalcanti@gmail.com>
2021-10-13 15:28:10 -0300

    exercicios arrays

---
+++ Fundamentos/B4/D2/index.js
@@ -0,0 +1,23 @@
+// exercicios de arrays
+
+// exercicio 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// exercicio 2
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// exercicio 3
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu3.push('Contato');
+
+console.log(menu3);
