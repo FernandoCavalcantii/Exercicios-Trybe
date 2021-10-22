1342edbc4b0348c363c69ffcd71a89822f3c1da1
fernando-cavalcanti <fafhcavalcanti@gmail.com>
2021-10-22 11:38:01 -0300

    exercio 1 calendario

--- Fundamentos/B5/D3/script.js
+++ Fundamentos/B5/D3/script.js
@@ -14,8 +14,18 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  
+  // exercicio 1
+
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-  for(i=0;i < dezDaysList; i++) {
-      
-  }
+  const ulDays = document.querySelector("#days");
+  
+
+  for(i=0;i < dezDaysList.length; i++) {
+    let days = document.createElement("li");
+    days.innerText = dezDaysList[i];
+    ulDays.appendChild(days);
+  };
+
+// exercicio 2
