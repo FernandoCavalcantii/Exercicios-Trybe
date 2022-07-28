c778c887cbe2a69983a0c3d92ed13ad4268944ee
fernando-cavalcanti <fafhcavalcanti@gmail.com>
2022-07-28 08:00:19 -0300

    Atualizando repositório até bloco 30.1

---
+++ Back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/fixacao/model/Book.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+interface IBook {
+  title: string,
+  author: string,
+}
+
+const bookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  }
+});
+
+// Para acessarmos os métodos disponibilizados pelo Mongoose(create, find, update...) e
+// implementarmos nossa API, criamos um model
+
+const bookModel = model<IBook>('Book', bookSchema);
