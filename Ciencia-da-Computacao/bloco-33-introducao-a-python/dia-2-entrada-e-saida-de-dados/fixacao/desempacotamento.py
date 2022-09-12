6bac23ae6c0861337980787de24864cfaf338090
Fernando Cavalcanti <fafhcavalcanti@gmail.com>
2022-09-12 18:13:45 -0300

    Exercicios aula 33.2

---
+++ Ciencia-da-Computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/fixacao/desempacotamento.py
@@ -0,0 +1,8 @@
+a, b = "cd"
+print(a)  # saída: c
+print(b)  # saída: d
+
+head, *tail = (1, 2, 3, 4)  # Quando um * está presente no desempacotamento,
+# os valores são desempacotados em formato de lista.
+print(head)  # saída: 1
+print(tail)  # saída: [2, 3]
